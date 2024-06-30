d9addd52bda2cad5fdb4fb97dda5fecd3369fbbb
Nathan JANCZEWSKI <nathan@janczewski.fr>
2024-06-30 16:37:04 +0200

    Update for 1.20.2 & 1.20.4
    Mostly working except:
    - Pressing escape to show the pause menu
    - Exiting chest requires to press it twice for some reason

--- src/main/java/fr/oxodao/fixescape/ClientEventHandler.java
+++ src/main/java/fr/oxodao/fixescape/ClientEventHandler.java
@@ -1,29 +1,56 @@
 package fr.oxodao.fixescape;
 
 import com.mojang.blaze3d.platform.InputConstants;
-
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class ClientEventHandler {
+    private static boolean IS_KEY_PRESSED = false;
+
     @SubscribeEvent
-    public void onKeyPress(InputEvent.Key evt) {
-        // We only care about the PRESS action
-        if (evt.getAction() != InputConstants.PRESS) {
+    public void onScreenKeyPressed(ScreenEvent.KeyPressed.Post evt){
+        if (ClientEventHandler.IS_KEY_PRESSED) {
             return;
         }
 
-        // We only care about OUR key
-        if (ClientModEvents.NEW_ESCAPE.get().getKey().getValue() != evt.getKey()) {
+        if (ClientModEvents.NEW_ESCAPE.get().getKey().getValue() != evt.getKeyCode()) {
             return;
         }
+
+        ClientEventHandler.IS_KEY_PRESSED = true;
 
         Minecraft mc = Minecraft.getInstance();
         if (mc.screen != null && mc.screen.shouldCloseOnEsc()) {
             mc.screen.onClose();
-        } else if (mc.screen == null) {
-            mc.pauseGame(false);
         }
+    }
+
+    @SubscribeEvent
+    public void onScreenKeyRelease(ScreenEvent.KeyReleased.Post evt){
+        ClientEventHandler.IS_KEY_PRESSED = false;
+    }
+
+    @SubscribeEvent
+    public void onKeyPress(InputEvent.Key evt) {
+        var isCorrectKey = ClientModEvents.NEW_ESCAPE.get().getKey().getValue() == evt.getKey();
+
+        if (evt.getAction() == InputConstants.RELEASE && ClientEventHandler.IS_KEY_PRESSED && isCorrectKey) {
+            ClientEventHandler.IS_KEY_PRESSED = false;
+            return;
+        } else if (evt.getAction() != InputConstants.PRESS || !isCorrectKey) {
+            return;
+        }
+
+        if (!ClientEventHandler.IS_KEY_PRESSED) {
+            ClientEventHandler.IS_KEY_PRESSED = true;
+
+            Minecraft mc = Minecraft.getInstance();
+            if (mc.screen == null) {
+                mc.pauseGame(false);
+            }
+        }
+
     }
 }
